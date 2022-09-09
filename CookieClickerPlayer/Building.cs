7244dd2222eeded67da6315f99c79c2f787d8b21
Steffan DeLong <thisbymaster@gmail.com>
2022-09-09 11:25:48 -0400

    updated to use WebManager instead of Webdriver directly, this will Autoupdate the chrome driver without me needing to manage it. Fixed issue with saving. Fixed issue with One Mind breaking progression.

---
+++ CookieClickerPlayer/Building.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieClickerPlayer
+{
+    class Building
+    {
+        public Building()
+        {
+        }
+        //public Building(int Index, decimal Percent, int Total)
+        //{
+        //    index = Index;
+        //    percent = Percent;
+        //    totalbought = Total;
+        //}
+        public Building(int Index, string tooltiphtml)
+        {
+            index = Index;
+            totalbought = int.Parse(tooltiphtml.Substring(tooltiphtml.IndexOf("</div><small>[owned : ") + 22, tooltiphtml.IndexOf("</small>]<div class=\"line\"></div>") - (tooltiphtml.IndexOf("</div><small>[owned : ") + 22)));
+            percent = totalbought==0?0:Decimal.Parse(tooltiphtml.Substring(tooltiphtml.IndexOf(" cookies per second (<b>")+24 , tooltiphtml.IndexOf("%</b> of total CpS)") -(tooltiphtml.IndexOf(" cookies per second (<b>") + 24)) );
+            
+        }
+        public int index;
+        public decimal percent;
+        public int totalbought;
+        public decimal Weight { get { return percent / totalbought; } }
+    }
+}
--- CookieClickerPlayer/Form1.cs
+++ CookieClickerPlayer/Form1.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace CookieClickerPlayer
 {
@@ -18,49 +20,63 @@ namespace CookieClickerPlayer
         static bool stopstart = false;
         static bool pause = false;
         private Thread thread;
+        public static ChromeDriver cd;
         public Form1()
         {
             InitializeComponent();
-            
+            new DriverManager().SetUpDriver(new ChromeConfig() {  });
+            cd = new ChromeDriver();
         }
 
         private async void button1_Click(object sender, EventArgs e)
         {
-            Thread t;
+            //Thread t;
             if (!stopstart)
             {
                 stopstart = true;
                 button1.Text = "Stop";
-                t = new Thread(T =>
+                thread = new Thread(T =>
                 {
                     RunCookieClickerThread();
                 })
                 { IsBackground = true };
-                t.Start();
+                thread.Start();
             }
             else
             {
                 stopstart = false;
+                Thread.Sleep(1000);
                 button1.Text = "Start";
                 //t.Abort();
+                //cd.Quit();
+                //t.Suspend();
+                //thread.Abort();
+                
             }
         }
 
         static async Task RunCookieClickerThread()
         {
-            ChromeDriver cd = new ChromeDriver();
+            
             cd.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(1);
             cd.Url = "https://orteil.dashnet.org/cookieclicker/";
+            cd.Manage().Window.Maximize();
+            Thread.Sleep(4000);
             try
             {
+                
+                cd.FindElementById("langSelect-EN").Click();
                 //load saved data.
                 if (System.IO.File.Exists(System.IO.Directory.GetCurrentDirectory() + "\\CookieSaveGame.txt"))
                 {
                     Thread.Sleep(2000);
                     cd.FindElementById("prefsButton").Click();
+                    Thread.Sleep(2000);
                     //cd.FindElementByLinkText("Import save");
                     cd.ExecuteScript("Game.ImportSave();");
+                    Thread.Sleep(2000);
                     cd.FindElementById("textareaPrompt").SendKeys(System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + "\\CookieSaveGame.txt"));
+                    Thread.Sleep(2000);
                     //cd.FindElementById("promptOption0").Click();
                     cd.ExecuteScript("Game.ImportSaveCode(l('textareaPrompt').value);Game.ClosePrompt();");
                     //cd.FindElementByCssSelector("div[class='X").Click();
@@ -68,63 +84,85 @@ namespace CookieClickerPlayer
                 }
                 while (stopstart)
                 {//Check for upgrades
-                    var activeupgrade = cd.FindElementsByCssSelector("div[class='crate upgrade enabled']");
-                    var activeBuildings = cd.FindElementsByCssSelector("div[class='product unlocked enabled']");
-                    var activeShimmers = cd.FindElementsByClassName("shimmer");
-                    
-                    if(activeShimmers.Count > 0)
-                    {
-                        try
-                        {
-                            activeShimmers.First().Click();
-                        }
-                        catch (Exception ex) 
-                        { 
-                            //if selecting shimmer failed.
-                        }
-                    }
-                    else if (activeupgrade.Count > 0)
-                    {
-                        try
-                        {
-                            activeupgrade.First().Click();
-                        }
-                        catch (Exception) { }
+                    try {
+                        var activeupgrade = cd.FindElementById("upgrades").FindElements(By.CssSelector("div[class='crate upgrade enabled']"));
+                        var activeBuildings = cd.FindElementsByCssSelector("div[class='product unlocked enabled']");
+                        var inactiveBuildings = cd.FindElementsByCssSelector("div[class='product unlocked disabled']");
+                        var activeShimmers = cd.FindElementsByClassName("shimmer");
+                        var CanCastgrimoire = cd.FindElementById("grimoireSpell1").GetAttribute("class") == "grimoireSpell titleFont ready";
 
-                    }
-                    else if (activeBuildings.Count > 0)//Check for building able to be bought
-                    {
-                        try
+                        if (activeShimmers.Count > 0)
                         {
-                            activeBuildings.First().Click();
+                            try
+                            {
+                                activeShimmers.First().Click();
+                            }
+                            catch (Exception ex)
+                            {
+                                //if selecting shimmer failed.
+                            }
                         }
-                        catch (Exception) { }
-                    }
-                    else
-                    {
-                        try { 
-                        cd.FindElementById("bigCookie").Click(); //Default to clicking the big cookie.
+                        else if (activeupgrade.Count > 0)
+                        {
+                            try
+                            {
+                                activeupgrade.First().Click();
+                            }
+                            catch (Exception) { }
+
                         }
-                        catch (Exception) { }
-                    }
-                    while(pause)
+                        else if (activeBuildings.Count() > 0 && inactiveBuildings.Count() <= 2)// && activeBuildings.Any(T => T.GetAttribute("id") == "product" + FindBestBuilding(cd, activeBuildings.ToList()).ToString()))//Check for building able to be bought
+                        {
+                            try
+                            {
+                                //activeBuildings.First(T => T.GetAttribute("id") == "product" + FindBestBuilding(cd, activeBuildings.ToList()).ToString()).Click();
+                                activeBuildings.Last().Click();
+                            }
+                            catch (Exception) { }
+                        }
+                        else if(CanCastgrimoire)
+                        {
+                            try
+                            {
+                                cd.FindElementById("grimoireSpell1").Click();
+                            }
+                            catch (Exception) { }
+                        }
+                        else
+                        {
+                            try {
+                                cd.FindElementById("bigCookie").Click(); //Default to clicking the big cookie.
+                            }
+                            catch (Exception) { }
+                        }
+                        while (pause)
+                        {
+                            
+                                Thread.Sleep(500);
+                        }
+                    } catch (Exception ex)
                     {
-                        Thread.Sleep(500);
+                        //if selecting shimmer failed.
                     }
                 }
+                 
                 cd.FindElementById("prefsButton").Click();
-                //cd.FindElementByLinkText("Export save");
-                cd.ExecuteScript("Game.ExportSave()");
-                string savedata = cd.FindElementById("textareaPrompt").Text;
-                cd.Quit();
-                System.IO.File.WriteAllText(System.IO.Directory.GetCurrentDirectory() + "\\CookieSaveGame.txt", savedata);
+                    //cd.FindElementByLinkText("Export save");
+                    Thread.Sleep(500);
+                    cd.ExecuteScript("Game.ExportSave()");
+                    Thread.Sleep(500);
+                    string savedata = cd.FindElementById("textareaPrompt").Text;
+                    cd.Quit();
+                    System.IO.File.WriteAllText(System.IO.Directory.GetCurrentDirectory() + "\\CookieSaveGame.txt", savedata);
                 
-            }
+        }
             catch( Exception ex)
             {
                 cd.FindElementById("prefsButton").Click();
                 //cd.FindElementByLinkText("Export save");
+                Thread.Sleep(500);
                 cd.ExecuteScript("Game.ExportSave()");
+                Thread.Sleep(500);
                 string savedata = cd.FindElementById("textareaPrompt").Text;
                 cd.Quit();
                 System.IO.File.WriteAllText(System.IO.Directory.GetCurrentDirectory() + "\\CookieSaveGame.txt", savedata);
@@ -135,6 +173,43 @@ namespace CookieClickerPlayer
         private void button2_Click(object sender, EventArgs e)
         {
             pause = !pause;
+            if (button2.Text == "Pause")
+            {
+                button2.Text = "UnPause";
+            }else if(button2.Text == "UnPause")
+            {
+                button2.Text = "Pause";
+            }
         }
+        private static int FindBestBuilding(ChromeDriver cd,List<IWebElement> activeBuildings )
+        {
+            List<Building> buildings = new List<Building>();
+            //for(int x=0;x <= 16;x++)
+            foreach(var b in activeBuildings)
+            {
+                
+                try
+                {
+                    int x = int.Parse(b.GetAttribute("id").Remove(0, 7));
+                    buildings.Add(new Building(x, cd.ExecuteScript("return Game.ObjectsById[" + x.ToString() + "].tooltip()").ToString()));
+                }
+                catch (Exception ex)
+                {
+                    //if selecting shimmer failed.
+                }
+            }
+            buildings.RemoveAll(T => T.percent == 0 || T.percent < 1);
+            if (buildings.Count > 0)
+            {
+                buildings.OrderByDescending(T => T.Weight);
+                return buildings.Last().index;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
     }
+
 }
